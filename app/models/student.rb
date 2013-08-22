c65cd4a801db0f62cd53b11801ecdf6e315bf789
Alex Fiore <writefiore@gmail.com>
2013-08-21 20:35:37 -0700

    All tests pass.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,20 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\w+\S*@\w+[\.]\w{2,}/i }
+  validates :age, numericality: { greater_than_or_equal_to: 5 }
+  validates :phone, format: { with: /\(?\d{3}\)?[-.\s]\d{3}[-.\s]\d{4}\s?x?\d*/ }
+
+  
+  def name 
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    ((Time.now - birthday.to_time).to_i / 31557600)
+  end
+
 end
+
